a45e808f6bb25a98d72b8c4df9b5da1161326bde
azizutm <101506425+azizutm@users.noreply.github.com>
2022-03-13 21:34:15 +0700

    Add files via upload

---
+++ coba2.php
@@ -0,0 +1,22 @@
+<?php
+
+function perkenalan(){
+  echo "Assalamulaikmu, ";
+  echo "Perkenalkan, nama saya aziz<br/>";
+  echo "Senang berkenalan dengan anda<br/>";
+}
+
+
+perkenalan();
+
+echo "<hr>";
+
+
+perkenalan();
+
+echo "<hr>";
+
+
+perkenalan();
+
+?>
