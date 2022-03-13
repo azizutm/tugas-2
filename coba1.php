2ad34bd1117e9d52a6071e584ec0aa55549ff23e
azizutm <101506425+azizutm@users.noreply.github.com>
2022-03-13 21:33:47 +0700

    Add files via upload

---
+++ coba1.php
@@ -0,0 +1,25 @@
+<?php
+    //OPERATOR ARITMATIKA
+    $a = 2;
+    $b = 3;
+    $c = 5;
+    $d = 10;
+ 
+    echo "<br/> PENJUMLAHAN <br/>";
+    echo $a+$b;
+    echo "<br/>";
+    $f = $c+$d;
+    echo $f;       
+    echo "<br/>";
+ 
+    echo "<br/> PENGURANGAN <br/>";
+    echo ($a-$d)."<br/>";
+    $f = $d-(-$c);
+    echo $f."<br/>";      
+    echo -$d."<br/>";     
+ 
+    echo "<br/> PERKALIAN <br/>";
+    echo ($a*$b)."<br/>"; 
+    echo ($b*$c)."<br/>";   
+ 
+?>
